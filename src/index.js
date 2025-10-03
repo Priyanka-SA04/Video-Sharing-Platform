c0cbbbbd9c6425a962ae78d6c37b48255c3400bf
Priyanka-SA04 <priyankasa2092004@gmail.com>
2025-10-03 20:06:07 +0530

    util codes added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import {app} from './app.js'
 dotenv.config({
     path: './.env'
 })
@@ -7,3 +8,14 @@ dotenv.config({
 
 
 connectDB()
+.then(()=>{         // promise is returned by the async function connectDB
+    app.on(("error",(error)=>{      //check for errors in express app connection with mongodb
+        console.log("error ", error);
+    }))
+    app.listen(process.env.PORT||8000,()=>{             //start server
+        console.log(`Server is running on port ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log("Mongodb connection error !!!", error);
+})
